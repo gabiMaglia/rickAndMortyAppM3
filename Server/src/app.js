03374bef3edf00e22c1b7eecb644c6ff22a80f73
Gabriel Maglia <gab.maglia@gmail.com>
2023-10-19 12:26:55 -0300

    login update started

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var morgan = require('morgan')
 const server = express();
 var cors = require('cors')
 const router = require('./routes/index')
@@ -6,9 +7,9 @@ const router = require('./routes/index')
 
 
 // MIDDLEWARES
-server.use(express.json());
-
 server.use(cors())
+server.use(express.json());
+server.use(morgan('tiny'))
 // ENDMIDDLEWARES
 
 server.use('/rickandmorty', router) 
--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,11 +1,17 @@
 const userList = require("../utils/users");
 
 const loginController = (req, res) => {
-  const { email, password } = req.query;
-
-  userList.find((user) => email === user.email && password === user.password)
-    ? res.status(200).json({ access: true })
-    : res.status(200).json({ access: false })
+  try {
+    const { email, password } = req.query;
+    if (!email || email === "" || !password || password === "") {
+      res.status(400).send("Faltan datos");
+    }
+    userList.find((user) => email === user.email && password === user.password)
+      ? res.status(200).json({ access: true })
+      : res.status(200).json({ access: false });
+  } catch (error) {
+    res.status(500).send(error.mensage);
+  }
 };
 
-module.exports = loginController
+module.exports = loginController;
